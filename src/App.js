a93b697377c6e2e0cd63456fad4929d2960b9ced
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-10 11:33:39 +0200

    Start work with login logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 
+const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
+const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`;
+
 function App() {
   function login() {
-    console.log("login");
+    fetch(
+      LOGIN_URL /* , {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    } */
+    );
   }
   return (
     <div>
