10e25ba75737986cdf5817161cbfcf399ed8e056
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-10 14:49:27 +0200

    Revome bad code.Add infrastructure for UserContext

--- src/pages/EventListPage.jsx
+++ src/pages/EventListPage.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 export default function EventListPage() {
+  const { token } = useContext(UserContext);
   return (
     <div>
       <h1>event list page</h1>
+      <p>{token}</p>
     </div>
   );
 }
