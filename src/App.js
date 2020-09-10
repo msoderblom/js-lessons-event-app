5ff244793ea28416316f523e955c39bf76bf60c1
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-10 14:17:50 +0200

    Remove old code

--- src/App.js
+++ src/App.js
@@ -3,28 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import EventListPage from "./pages/EventListPage";
 
-const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
-const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`;
-
 function App() {
-  const [email, setEmail] = useState("test.user@willandskill.se");
-  const [password, setPassword] = useState("js-lesson-10");
-
-  function login() {
-    const payload = {
-      email: email,
-      password: password,
-    };
-    fetch(LOGIN_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }
   return (
     <div>
       <Switch>
