85d3c0ca43614cb2c86da9473169a684f35b0413
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-10 11:34:20 +0200

    Add ref to input fields

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
 const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`;
 
 function App() {
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
   function login() {
     fetch(
       LOGIN_URL /* , {
@@ -21,11 +23,15 @@ function App() {
       <div>
         <div>
           <label htmlFor="email">Email</label>
-          <input name="email" placeholder="jane.doe@company.com" />
+          <input
+            ref={emailInput}
+            name="email"
+            placeholder="jane.doe@company.com"
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input name="password" type="password" />
+          <input ref={passwordInput} name="password" type="password" />
         </div>
         <button onClick={login}>Login</button>
       </div>
